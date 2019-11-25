16602a8f009f9bb70444e225fe32657fcac8a0d6
Brad Larson <bhlarson@gmail.com>
2019-11-24 16:18:40 -0800

    Setting up relaypy project

---
+++ server.js
@@ -0,0 +1,42 @@
+﻿console.log("Starting RelayPi on " + process.platform + " with node version " + process.version);
+
+require('dotenv').config({ path: './config.env' });
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var SolarCalc = require('solar-calc');
+var schedule = require('node-schedule');
+var mysql = require('mysql');
+
+console.log("External Dependencies Found");
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.dbhost,
+    user: process.env.dbuser,
+    //password: 'password',
+    database: process.env.dbname
+});
+
+
+console.log("mysql.createPool exists=" + (typeof pool !== 'undefined'));
+
+var port = process.env.PORT || 1337;
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html')
+});
+
+
+http.listen(port, function () {
+});
+
+var solar = new SolarCalc(new Date(), 45.5, -122.8);
+console.log("sunrise Today: ", solar.sunrise.toString());
+
+var solar = new SolarCalc(new Date(), 45.5, -122.8);
+console.log("sunrise ", solar.sunrise);
+
+module.exports = app;
+console.log("Creation  successful")
