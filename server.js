3b6cde36652c275e89a156e52d4e665008427d54
Brad Larson <bhlarson@gmail.com>
2019-12-13 12:19:05 -0800

    Test actual relay

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ var SunCalc = require('suncalc2');
 var schedule = require('node-schedule');
 var mysql = require('mysql');
 var cronparser = require('cron-parser');
-//const Gpio = require('onoff').Gpio;
+const Gpio = require('onoff').Gpio;
 
-rly1.writeSync(1)
+const rly1 = new Gpio(26, 'out');
+const rly2 = new Gpio(20, 'out');
+const rly3 = new Gpio(21, 'out');
+
+rly1.writeSync(1);
+console.log('rly1=1');
 
 var schedule = [
   { timer: 'time', config: { timestamp: 1577099015 }, conditions: [], action: () => { rly1.writeSync(1) } },
@@ -47,7 +52,7 @@ function NextEvent(timestamp, schedule){
   });
 }
 
-NextEvent(time.time(), schedule);
+//NextEvent(time.time(), schedule);
 
 console.log("External Dependencies Found");
 
@@ -87,32 +92,32 @@ var lunar = SunCalc.getMoonTimes(new Date(), process.env.latitude, process.env.l
 console.log("Sunrise Today: ", solar.sunrise.toString());
 console.log("Sunset Today: ", solar.sunset.toString());
 
-var j = schedule.scheduleJob('*/5 * * * * *', function(){
-    console.log(new Date() + ' Repeat');
+//var j = schedule.scheduleJob('*/5 * * * * *', function(){
+//    console.log(new Date() + ' Repeat');
 
     //rly1.read()
     //    .then(value => rly1.write(value ^ 1))
     //    .catch(err => console.log(err));   
-  }
-);
+//  }
+//);
 
-var k = schedule.scheduleJob('*/10 * * * * *', function(){
-    console.log(new Date() + ' Repeat');
+//var k = schedule.scheduleJob('*/10 * * * * *', function(){
+//    console.log(new Date() + ' Repeat');
 
     //rly2.read()
     //    .then(value => rly2.write(value ^ 1))
     //    .catch(err => console.log(err));   
-  }
-);
+//  }
+//);
 
-var l = schedule.scheduleJob('*/20 * * * * *', function(){
-    console.log(new Date() + ' Repeat');
+//var l = schedule.scheduleJob('*/20 * * * * *', function(){
+//    console.log(new Date() + ' Repeat');
 
     //rly3.read()
     //    .then(value => rly3.write(value ^ 1))
     //    .catch(err => console.log(err));   
-  }
-);
+//  }
+//);
 
 module.exports = app;
 console.log("Created RelayPi server")
